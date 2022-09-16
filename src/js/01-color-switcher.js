dbd94ee27e963025ff7737db7d34528e1a7ec844
Polina Igrushkina <polina.igrushkina@gmail.com>
2022-09-16 19:53:25 +0300

    Solved task-01

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,31 @@
+const startBtnEl = document.querySelector('[data-start]');
+const stopBtnEl = document.querySelector('[data-stop]');
 
+const bodyEl = document.querySelector('body');
+
+//Добавляет событие на кнопку Старт
+startBtnEl.addEventListener('click', onStartBtnClick);
+
+function onStartBtnClick() {
+    timerId = setInterval(() => {
+    
+    bodyEl.style.backgroundColor = getRandomHexColor();
+    startBtnEl.setAttribute('disabled', true);
+    stopBtnEl.removeAttribute('disabled');
+      
+  }, 1000);
+};
+
+//Добавляет событие на кнопку Стоп
+stopBtnEl.addEventListener('click', onStopBtnClick);
+
+function onStopBtnClick() {
+  clearInterval(timerId);
+  stopBtnEl.setAttribute('disabled', true);
+  startBtnEl.removeAttribute('disabled');
+};
+
+//Функция генерации случайного цвета
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+};
